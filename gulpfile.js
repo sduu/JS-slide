bd788003ef5a56ee8f3fb9d3b6aacf9dc6861fb4
hyesu00 <46313348+hyesu00@users.noreply.github.com>
2021-11-25 17:18:29 +0900

    Chore : gulp-autoprefixer 모듈 추가

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const babel = require("gulp-babel"),
 	  concat = require('gulp-concat'),
 	  uglify = require('gulp-uglify'),
 	  rename = require('gulp-rename'),
-	  minificss = require('gulp-minify-css');
+	  minificss = require('gulp-minify-css'),
+	  autoprefixer = require('gulp-autoprefixer');
 
 /* js 병합 */
 gulp.task('combine:js', (done) => {
@@ -20,6 +21,7 @@ gulp.task('combine:js', (done) => {
 gulp.task('combine:css', (done) => {
 	gulp
 		.src(['src/css/**/*.css'])
+		.pipe(autoprefixer())
 		.pipe(minificss())
 		.pipe(rename('jsSlide.min.css'))
 		.pipe(gulp.dest('dist/css'));
